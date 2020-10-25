a4c52ff6786a4f24ec47cae65cb9a1aeba3b764e
Anhelina <kulakovaanhelina@gmail.com>
2020-10-25 23:13:38 +0100

    3.2: Phonebook backend step2

---
+++ part3/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+
+var d = new Date()
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+]
+
+
+
+
+app.get('/', (request, response) => {
+  response.send('<div><p>Phonebook has info of ' + persons.length + '</p>' + d + '</div>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
