7c7ba0298084bf613d058e153aa13e38e710fe4e
Anhelina <kulakovaanhelina@gmail.com>
2020-11-10 21:40:00 +0100

    ex 3.9

--- part3/index.js
+++ part3/index.js
@@ -3,6 +3,9 @@ var express = require('express')
 var morgan = require('morgan')
 
 var bodyParser = require('body-parser');
+const cors = require('cors')
+
+app.use(cors())
 
 var app = express()
 
@@ -97,7 +100,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
