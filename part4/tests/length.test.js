348b86376c0e9f7289da1fd30578e04493a6bf43
Anhelina <kulakovaanhelina@gmail.com>
2020-12-01 16:58:28 +0100

    ex 4.4

--- part4/tests/length.test.js
+++ part4/tests/length.test.js
@@ -1,7 +1,7 @@
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = ["e"]
+  const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,8 +1,17 @@
 const dummy = (blogs) => {
   return blogs.length === 0
     ? 1
-    : blogs.length/blogs.length
+    : blogs.length / blogs.length
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  const result = blogs.map((blog) => blog.likes)
+  return result.reduce(reducer, 0)
 }
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
