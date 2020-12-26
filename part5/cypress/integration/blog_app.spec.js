2923ae1777ed7c4d6e0b5eb5c8963e453e9cb9f2
Anhelina <kulakovaanhelina@gmail.com>
2020-12-26 19:23:07 +0100

    5.18: bloglist end to end testing, step2

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -1,24 +1,41 @@
 describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', 'http://localhost:3001/api/testing/reset/')
     const user = {
-      name: 'Saimon Berry',
-      username: 'aimon',
+      name: 'Milla Boon',
+      username: 'milka',
       password: 'salainen'
     }
     cy.request('POST', 'http://localhost:3001/api/users/', user)
+      .its('body')
     cy.visit('http://localhost:3000')
-
 
 
   })
 
   it('Login form is shown', function () {
     cy.contains('login').click()
-    cy.get('#username').type('saimon')
-    cy.get('#password').type('salainen')
-    cy.get('#login-button').click()
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
-    cy.contains('Saimon Berry logged-in')
+  })
+
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('milka')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('Milla Boon logged-in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('bugai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.error').should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+      cy.get('html').should('not.contain', 'Milla Boon logged-in')
+    })
   })
 })
