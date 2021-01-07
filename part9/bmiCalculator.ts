9b760a609f1717f763138a1dd4bdf8835002b7ec
Anhelina Kulakova <kulakovaanhelina@gmail.com>
2021-01-07 18:12:41 +0100

    cmd

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,14 +1,45 @@
-type BMI = string;
+interface BmiText {
+  bmi: string
+  }interface BmiValues {
+ height: number;
+ weight: number;
+}
 
-const calculateBmi = (h: number, w: number): string => {
-  let bmi = Number(((w / h / h) * 10000).toFixed(2))
+const parseArguments = (args: Array<string>):BmiValues  => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
 
-  if (bmi < 17) {
-    return 'Underweight';
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return 'Normal (healthy weight)';
-  } else if (bmi >= 25) {
-    return 'Overweight'
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+     height: Number(args[2]),
+     weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
 }
-console.log(calculateBmi(180, 74))
+const calculateBmi =  (h: number, w: number) : string => {
+  let bmi = Number(((w / h / h) * 10000).toFixed(2))
+  let result = ''
+  if (bmi < 17) {
+    result = 'Underweight'
+    console.log(result);
+    return result
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    result ='Normal (healthy weight)'
+    console.log(result);
+    return result
+  } else if (bmi >= 25) {
+    result = 'Overweight'
+    console.log(result);
+    return result
+  }
+
+}
+
+try {
+  const {height,weight } = parseArguments(process.argv);
+ calculateBmi(height,weight)
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
