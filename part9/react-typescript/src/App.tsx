3693e2c2afd335d9e13ce39b9f82d691c00e47db
Anhelina Kulakova <kulakovaanhelina@gmail.com>
2021-01-16 19:28:54 +0100

    drafted

--- part9/react-typescript/src/App.tsx
+++ part9/react-typescript/src/App.tsx
@@ -1,20 +1,63 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 
-
-interface CoursesProps {
-  name: string,
-  exerciseCount: number
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartBase, CoursePartDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBase, CoursePartDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
 const Header: React.FC<{ name: string }> = ({ name }) => (
   <h1>{name}</h1>
 );
 
-const Content: React.FC<{ courses: Array<CoursesProps> }> = ({ courses }) => (
-  // eslint-disable-next-line react/jsx-key
-  <p>{courses.map(course => <p>{course.name} {course.exerciseCount}</p>)}</p>
+const Part: React.FC<{ courseParts: CoursePart }> = ({courseParts }) => {
+  switch (courseParts.name) {
+    case "Fundamentals":
+      return <p>{courseParts.name} {courseParts.exerciseCount} {courseParts.description} </p>
+    case "Using props to pass data":
+      return <p>{courseParts.name} {courseParts.exerciseCount}</p>
+    case "Deeper type usage":
+      return <p>{courseParts.name} {courseParts.exerciseCount}
+      {courseParts.description} {courseParts.exerciseSubmissionLink}</p>
+    default:
+      return assertNever(courseParts)
+  }
+
+}
+
+const Content: React.FC<{ courses: Array<CoursePartBase> }> = ({ courses })  => (
+  <div>
+  {courses.map(course => <Part courseParts = {course}/>)}
+  </div>
 );
 
-const Total: React.FC<{ courses: Array<CoursesProps> }> = ({ courses }) => (
+
+const Total: React.FC<{ courses: Array<CoursePartBase> }> = ({ courses }) => (
   // eslint-disable-next-line react/jsx-key
   <p>
     Number of exercises{" "}
@@ -23,23 +66,27 @@ const Total: React.FC<{ courses: Array<CoursesProps> }> = ({ courses }) => (
 );
 
 
-
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
     }
-  ];
+  ]
 
   return (
     <div>
