dad5bfd040d4fabc239d73f16ad6342d503237bc
Anhelina Kulakova <kulakovaanhelina@gmail.com>
2021-01-18 22:33:11 +0100

    7.4: anecdotes and hooks step1

--- part7/routed-anecdotes/src/components/Createnew.jsx
+++ part7/routed-anecdotes/src/components/Createnew.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import {useField} from '../hooks/index'
 
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content  = useField('content')
+  const author  = useField('author')
+  const info  = useState('info')
 
 
   const handleSubmit = (e) => {
@@ -23,15 +24,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input  {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
