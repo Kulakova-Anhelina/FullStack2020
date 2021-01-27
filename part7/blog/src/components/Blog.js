3c2b7d2ca16bf8e9f2f700db1545cfa706cc3388
Anhelina Kulakova <kulakovaanhelina@gmail.com>
2021-01-27 21:45:13 +0100

    code clean up

--- part7/blog/src/components/Blog.js
+++ part7/blog/src/components/Blog.js
@@ -1,15 +1,7 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import blogReducer from '../reducers/blogReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
-
-const Blog = () => {
-  const [visible, setVisible] = useState(false)
-  const dispatch = useDispatch()
-  const blogState = useSelector(state => state.blogs)
-  console.log(blogState, "blog from Blog")
-
+const Blog = ({ blog }) => {
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -17,26 +9,39 @@ const Blog = () => {
     borderWidth: 1,
     marginBottom: 5
   }
-
+  const [visible, setVisible] = useState(false)
   const label = visible ? 'hide' : 'view'
+
+  return(
+    <>
+    <div style={blogStyle} className='blog'>
+      <i>{blog.title}</i> by {blog.author}
+      <button onClick={() => setVisible(!visible)}>{label}</button>
+      </div>
+      {visible&&(
+      <div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}
+        </div>
+      </div>
+    )}
+  </>
+  )
+}
+
+
+const Blogs = () => {
+
+  const dispatch = useDispatch()
+  const blogState = useSelector(state => state.blogs)
+  console.log(blogState, "blog from Blog")
   const byLikes = (b1, b2) => b2.likes - b1.likes
   return (
     <>
       {blogState.sort(byLikes).map((blog) =>
-        <>
-          <div style={blogStyle} className='blog'>
-            <i>{blog.title}</i> by {blog.author}
-            <button onClick={() => setVisible(!visible)}>{label}</button>
-          </div>
-          {visible && (
-            <div>
-              <div>{blog.url}</div>
-              <div>likes {blog.likes}
-              </div>
-            </div>
-          )}
-        </>
-
+       <Blog
+       blog={blog}
+       />
       )}
 
 
@@ -44,4 +49,4 @@ const Blog = () => {
   )
 }
 
-export default Blog
+export default Blogs
