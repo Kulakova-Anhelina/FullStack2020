cefd7b9748e3d0679162b3042f0b101f0f200910
Anhelina Kulakova <kulakovaanhelina@gmail.com>
2021-03-22 21:28:55 +0100

    7.19: Styles, step1

--- part7/blog/src/components/Blog.js
+++ part7/blog/src/components/Blog.js
@@ -14,24 +14,29 @@ const Blog = ({ blogs, handleLike, handleRemove }) => {
     marginBottom: 5
   }
   const id = useParams().id
-  const blog = blogs.find(n => n.id ===id)
+  const blog = blogs.find(n => n.id === id)
   if (!blog) {
     return null
   }
 
+  console.log(blog)
   return (
     <>
       <div style={blogStyle} className='blog'>
         <i>{blog.title}</i> by {blog.author}
       </div>
-        <div>
-          <div>{blog.url}</div>
-            <div>likes {blog.likes}
-              <button onClick={() =>handleLike(blog.id)}>like</button>
-            </div>
-            <div>{blog.user.name}</div>
-            {<button onClick={() => handleRemove(blog.id)}>remove</button>}
-          </div>
+      <div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}
+          <button onClick={() => handleLike(blog.id)}>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+        {<button onClick={() => handleRemove(blog.id)}>remove</button>}
+      </div>
+      <h4>Comments</h4>
+      <ul>
+        <li>{blog.comments}</li>
+      </ul>
     </>
   )
 }
