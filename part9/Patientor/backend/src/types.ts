e2b4f51467ad7042c14173dd02fb92c57d07a96a
Anhelina Kulakova <kulakovaanhelina@gmail.com>
2021-04-19 22:37:20 +0200

    9.19: patientor, step4

--- part9/Patientor/backend/src/types.ts
+++ part9/Patientor/backend/src/types.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+
 export enum Gender {
   Male = 'male',
   Female = 'female',
@@ -13,13 +12,59 @@ export interface Diagnoses {
   latin?: string;
 }
 export interface Patient {
-  id: string ;
+  id: string;
   name: string;
   dateOfBirth: string;
   ssn: string;
   gender: Gender;
   occupation: string;
-  entries: Entry[]
+  entries: Entry[];
 }
-export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
-export type NewPatientEntry = Omit<Patient, 'id' >;
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnoses['code']>;
+}
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+type Discharge = {
+  date?: string;
+  criteria?: string;
+};
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+type SickLeave = {
+  startDate: string;
+  endDate: string;
+};
+
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName?: string;
+  sickLeave?: SickLeave;
+}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
+export type NewPatientEntry = Omit<Patient, 'id'>;
